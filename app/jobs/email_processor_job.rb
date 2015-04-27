5984e4ef30a177852a1065d785f3643697ff3933
Keith McDonnell <keith@dancingtext.com>
2015-04-27 17:25:22 +0100

    /email_processor -> fix: testing json

--- app/jobs/email_processor_job.rb
+++ app/jobs/email_processor_job.rb
@@ -1,7 +1,8 @@
 class EmailProcessorJob < ActiveJob::Base
   queue_as :default
   
-  def perform(email)
+  def perform(email_json)
+    email = JSON.parse email_json
     Enquiry.new(email).save
     Reply.new(email).save
     Command.new(email).execute
