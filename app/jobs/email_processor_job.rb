f4946becc023d5dd6efd019fef666e7fcfd689c4
Keith McDonnell <keith@dancingtext.com>
2015-04-28 10:57:24 +0100

    /email_processor -> fix: email (not hash) arg

--- app/jobs/email_processor_job.rb
+++ app/jobs/email_processor_job.rb
@@ -2,7 +2,7 @@ class EmailProcessorJob < ActiveJob::Base
   queue_as :default
   
   def perform(email_json)
-    email = JSON.parse email_json
+    email = Griddler::Email.new JSON.parse(email_json)
     Enquiry.new(email).save
     Reply.new(email).save
     Command.new(email).execute
