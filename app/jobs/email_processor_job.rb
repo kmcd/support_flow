724f6ab34a4e8ea62f9fdc1994515399b984747f
Keith McDonnell <keith@dancingtext.com>
2015-05-06 17:22:29 +0100

    /email_processor -> fix: unsure of mandrill format

--- app/jobs/email_processor_job.rb
+++ app/jobs/email_processor_job.rb
@@ -2,8 +2,7 @@ class EmailProcessorJob < ActiveJob::Base
   queue_as :default
   
   def perform(email_json)
-    throw JSON.parse(email_json)
-    # email = Griddler::Email.new 
+    email = Griddler::Email.new JSON.parse(email_json['params'])
     
     # Enquiry.new(email).save
     # Reply.new(email).save
