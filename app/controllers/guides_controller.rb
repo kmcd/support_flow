ba1defbbd0de7713cc7ca20ddb00ad443fd9ba57
Keith McDonnell <keith@dancingtext.com>
2015-06-23 15:27:22 +0100

    /guides/1 -> crud OK. closes #92

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,55 +1,65 @@
 class GuidesController < ApplicationController
   skip_before_filter :require_login, only:%i[ public ]
-  before_action :set_guide, only: [:update, :show, :edit, :destroy]
-  
+  before_action :set_guide, only: %i[ update show edit destroy ]
+
   def index
     @guides = Guide.pages current_team
   end
-  
+
   def new
-    @guide = current_team.guides.new
+    @guide = Guide.new
   end
-  
+
   def create
     @guide = current_team.guides.new guide_params
-    
+
     if @guide.save
-      flash[:created] = true
-      render text:"window.location.replace('#{guide_path(@guide)}')"
+      redirect_to guide_path(@guide)
+    else
+      render :new
     end
   end
-  
+
   def show
   end
   
-  def update
-    @guide.update guide_params
+  def edit
   end
-  
+
+  def update
+    if @guide.update guide_params
+      redirect_to team_guides_path(current_team)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @guide.delete
     redirect_to guides_path
   end
-  
+
   def public
     public_guide = PublicGuide.new params[:team], params[:guide]
-    
+
     if public_guide.present?
       public_guide.increment_view_count current_agent
-      render text:public_guide.html # OPTIMIZE: page caching
+      render text:public_guide.html
     else
-      # TODO: bootstrap 404 pages
       render file:"#{Rails.root}/public/404", layout:false, status: :not_found
     end
   end
-  
+
   private
-  
+
   def set_guide
-    @guide = Guide.find params[:id] # TODO: change to find by slug
+    @guide = Guide.find params[:id]
   end
 
   def guide_params
-    params.require(:guide).permit %i[ name content ]
+    params.require(:guide).permit %i[
+      name
+      content
+    ]
   end
 end
