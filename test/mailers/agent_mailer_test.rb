70882ac8742f53ae1ec36c6659db480b9a2721ad
Keith McDonnell <keith@dancingtext.com>
2015-07-22 13:53:33 +0100

    /email/outbound -> moving integration tests

--- test/mailers/agent_mailer_test.rb
+++ test/mailers/agent_mailer_test.rb
@@ -1,12 +1,4 @@
 require 'test_helper'
 
 class AgentMailerTest < ActionMailer::TestCase
-  test "reply" do
-    skip
-    mail = AgentMailer.reply
-    assert_equal "Reply", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
 end
