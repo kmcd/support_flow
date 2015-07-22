77c8797c9b37da622e4df4482d36c06d88d3dd0d
Keith McDonnell <keith@dancingtext.com>
2015-07-22 13:52:16 +0100

    /email/outbound -> moving integration tests

--- test/controllers/email/attachments_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Email::AttachmentsControllerTest < ActionController::TestCase
-  test "should get destroy" do
-    skip
-    get :destroy
-    assert_response :success
-  end
-end
