80fd3ba54208f5b8b548a6954cb9c2bddfd996e4
Keith McDonnell <keith@dancingtext.com>
2015-09-03 15:32:20 +0100

    /signup -> bootstrap default guide layout

--- app/jobs/demo_job.rb
+++ app/jobs/demo_job.rb
@@ -173,7 +173,7 @@ class DemoJob < ActiveJob::Base
   def create_guides
     team.guides.create \
       name: '_template',
-      content: "<html><!-- content --></html>",
+      content: guide_template,
       current_agent:sample_agent
 
     team.guides.create \
@@ -278,5 +278,43 @@ class DemoJob < ActiveJob::Base
       label:60..90,
       close:50..70
     }
+  end
+  
+  def guide_template
+    <<-TEMPLATE.strip_heredoc
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+      
+          <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+          <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+          <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+          <![endif]-->
+          
+          <title>#{team.name}</title>
+        </head>
+        <body>
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-9 col-xs-12">
+                <!-- content -->
+              </div>
+            </div>
+          </div>
+          
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+          <script src="js/bootstrap.min.js"></script>
+        </body>
+      </html>
+    TEMPLATE
   end
 end
