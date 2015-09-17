a2a72313d9034e81b8f1d59057182ef755f7aac0
Keith McDonnell <keith@dancingtext.com>
2015-09-17 15:33:44 +0100

    /app -> fix: delayed job error

--- app/jobs/sales_sync_job.rb
+++ app/jobs/sales_sync_job.rb
@@ -1,5 +1,4 @@
 class SalesSyncJob < ActiveJob::Base
-  SALES_DB = Sequel.connect "postgres://xdukemdiocmvih:kg6aasRN7aj2YWALNOhIltdj_N@ec2-54-217-202-109.eu-west-1.compute.amazonaws.com:5432/d44t623isgoibt"
   queue_as :default
 
   def perform(*args)
@@ -7,10 +6,12 @@ class SalesSyncJob < ActiveJob::Base
     sync_opportunities
     sync_ratings
   end
-  
+
+  private
+
   def sync_leads
     leads.each do |agent|
-      lead_id = SALES_DB[:leads].insert \
+      lead_id = sales_db[:leads].insert \
         email:agent.email_address,
         first_name:"#{ agent.name || 'agent' }",
         last_name:"##{agent.id}",
@@ -18,7 +19,7 @@ class SalesSyncJob < ActiveJob::Base
         created_at:0.minutes.ago,
         updated_at:0.minutes.ago
 
-      SALES_DB[:comments].insert \
+      sales_db[:comments].insert \
         user_id:1,
         commentable_type:'Lead',
         commentable_id:lead_id,
@@ -27,14 +28,14 @@ class SalesSyncJob < ActiveJob::Base
         created_at:0.minutes.ago,
         updated_at:0.minutes.ago
 
-      contact_id = SALES_DB[:contacts].insert \
+      contact_id = sales_db[:contacts].insert \
         email:agent.email_address,
         first_name:"#{ agent.name || 'agent' }",
         last_name:"##{agent.id}",
         created_at:0.minutes.ago,
         updated_at:0.minutes.ago
 
-      SALES_DB[:comments].insert \
+      sales_db[:comments].insert \
         user_id:1,
         commentable_type:'Contact',
         commentable_id:contact_id,
@@ -45,20 +46,20 @@ class SalesSyncJob < ActiveJob::Base
       end
     end
   end
-  
+
   def sync_opportunities
     opportunities.each do |agent|
-      SALES_DB[:leads].
+      sales_db[:leads].
         where( email:agent.email_address ).
         update status:'converted'
 
-      account_id = SALES_DB[:accounts].insert \
+      account_id = sales_db[:accounts].insert \
         name:"##{agent.team.id} #{agent.team.name}",
         category:'Customer',
         created_at:0.minutes.ago,
         updated_at:0.minutes.ago
 
-      SALES_DB[:comments].insert \
+      sales_db[:comments].insert \
         user_id:1,
         commentable_type:'Account',
         commentable_id:account_id,
@@ -67,20 +68,20 @@ class SalesSyncJob < ActiveJob::Base
         created_at:0.minutes.ago,
         updated_at:0.minutes.ago
 
-      opportunity_id = SALES_DB[:opportunities].insert \
+      opportunity_id = sales_db[:opportunities].insert \
         name:"##{agent.team.id} #{agent.team.name} TRIAL",
         stage:'trial',
         campaign_id:agent.campaign_id,
         created_at:0.minutes.ago,
         updated_at:0.minutes.ago
 
-      SALES_DB[:account_opportunities].insert \
+      sales_db[:account_opportunities].insert \
         account_id:account_id,
         opportunity_id:opportunity_id,
         created_at:0.minutes.ago,
         updated_at:0.minutes.ago
 
-      SALES_DB[:comments].insert \
+      sales_db[:comments].insert \
         user_id:1,
         commentable_type:'Opportunity',
         commentable_id:opportunity_id,
@@ -90,20 +91,20 @@ class SalesSyncJob < ActiveJob::Base
         updated_at:0.minutes.ago
     end
   end
-  
+
   def sync_ratings
     ratings.each do |address, rating|
-      SALES_DB[:leads].where(email:address).update rating:rating
+      sales_db[:leads].where(email:address).update rating:rating
     end
   end
-  
+
   def leads
     Agent.
       support_flow_customers.
       demo.
       last_5_mins
   end
-  
+
   def opportunities
     Agent.
       support_flow_customers.
@@ -112,11 +113,16 @@ class SalesSyncJob < ActiveJob::Base
       group_by(&:team_id).
       map {|_, agents| agents.first }
   end
-  
+
   def ratings
     Login.group(:email_address).count.map do |address, count|
       rating = count > 5 ? 5 : count
       [ address, rating ]
+  end
+  
+  def sales_db
+    @sales_db ||= Sequel.connect \
+      "postgres://xdukemdiocmvih:kg6aasRN7aj2YWALNOhIltdj_N@ec2-54-217-202-109.eu-west-1.compute.amazonaws.com:5432/d44t623isgoibt"
   end
 end
 
